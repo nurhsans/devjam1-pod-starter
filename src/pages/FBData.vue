2b7b0a3b1551cd365c23a184f4ed5ce67d3b4384
Nur Hidayah Sanwari <nurhsans@gmail.com>
2020-06-07 23:06:29 +0800

    POD: Works in getting and rendering organizers data

---
+++ src/pages/FBData.vue
@@ -0,0 +1,45 @@
+<template>
+  <main class="mx-auto max-w-6xl">
+    <h1 class="text-3xl font-semibold">Firebase Data Render</h1>
+
+     
+    <p>{{orgData}}</p>
+    <div v-for="(org, i) in storageOrg" :key="i" class="sm:mt-0 sm:w-80 sm:flex-shrink-0 mx-5 my-6">
+            <p>hello {{org.name}} : {{org.id}}</p>
+        </div>
+
+    
+
+   <LazyHydrate when-idle>
+    </LazyHydrate>
+
+  </main>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+     orgData: 'hello'
+    }
+  },
+  metaInfo: {
+    title: 'Firebase Data'
+  },
+  methods: {
+    // getOrgData() {
+    //   console.log("get org data function")
+    // },
+  },
+  created() {
+    console.log(this.$store)
+  },
+  computed: {
+    storageOrg() {
+      return this.$store.organizers
+    }
+  },
+    components: {
+  }
+}
+</script>
---
+++ src/store.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import Storage from './local'
+
+const store = Vue.observable({
+  __bindings: {}, // DO NOT REMOVE
+
+  documents: null,
+  organizers: null,
+  clicks: 0
+})
+
+/* Local storage */
+store.$storage = new Storage('gridfire')
+
+/* Example of a function */
+store.CLICK = () => {
+  console.log('clicking')
+  store.clicks = store.clicks + 1
+}
+
+
+Vue.prototype.$store = store
+
+export default store
